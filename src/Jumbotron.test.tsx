96334e0d19d1fe1105a06c57e94b3edd8eb6daf9
sungeun101 <josungeun1019@gmail.com>
2022-06-02 14:25:33 +0900

    Assert expected behavior

--- src/Jumbotron.test.tsx
+++ src/Jumbotron.test.tsx
@@ -4,5 +4,7 @@ import Jumbotron from "./Jumbotron";
 it("displays the heading", () => {
   render(<Jumbotron />);
   // screen.getByText(/welcome to our site!/i);
-  screen.getByRole("heading", { name: /welcome to our site!/i });
+  expect(
+    screen.getByRole("heading", { name: /welcome to our site!/i })
+  ).toBeInTheDocument();
 });
