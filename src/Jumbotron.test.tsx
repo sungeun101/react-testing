1b6f49c204d595a22c07dc167ad3dabb6b2654ff
sungeun101 <josungeun1019@gmail.com>
2022-06-02 14:05:21 +0900

    select elements in the component DOM output

--- src/Jumbotron.test.tsx
+++ src/Jumbotron.test.tsx
@@ -1,6 +1,8 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Jumbotron from "./Jumbotron";
 
 it("displays the heading", () => {
   render(<Jumbotron />);
+  // screen.getByText(/welcome to our site!/i);
+  screen.getByRole("heading", { name: /welcome to our site!/i });
 });
